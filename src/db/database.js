013e20bc4cc11a6be8ff5965484e67283fc92986
Joao Marcelo <jm.castro@usp.br>
2022-07-03 02:47:48 -0300

    feat: add database connection

---
+++ src/db/database.js
@@ -0,0 +1,18 @@
+import { MongoClient, ObjectId } from "mongodb";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+let db;
+const mongoClient = new MongoClient(process.env.DATABASE_URL);
+const promise = mongoClient.connect();
+
+promise
+  .then(() => {
+    db = mongoClient.db(process.env.DATABASE_NAME);
+    console.log("Connected to database!");
+  })
+  .catch((err) => console.error(err));
+
+const ObjectId = ObjectId;
+export { db, ObjectId };
