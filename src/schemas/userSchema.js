42bdd492002e1caf204f21537f81061b6a26f521
Joao Marcelo <jm.castro@usp.br>
2022-07-04 16:02:24 -0300

    chore: adjust code for deploy on heroku

--- src/schemas/userSchema.js
+++ src/schemas/userSchema.js
@@ -1,7 +1,7 @@
 import joi from "joi";
 
 export const signUpUserSchema = joi.object({
-  name: joi.string().min(1).required(),
+  name: joi.string().required(),
   email: joi.string().email().required(),
   password: joi.string().required(),
   confirmPassword: joi.ref("password"),
