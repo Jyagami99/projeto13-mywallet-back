1268b1e95bc4130cf2b901c0b3c7af22f191ec17
João Marcelo Simão de Castro <jm.castro@usp.br>
2024-06-01 14:49:45 -0300

    fix: fix transactionSchema

--- src/schemas/transactionSchema.js
+++ src/schemas/transactionSchema.js
@@ -3,5 +3,5 @@ import joi from "joi";
 export const transactionSchema = joi.object({
   type: joi.string().required(),
   description: joi.string().required(),
-  value: joi.string().required(),
+  value: joi.number().required(),
 });
